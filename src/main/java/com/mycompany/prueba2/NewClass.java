a181ffdbabe7807d5a54eccdc07a923f2d625c70
francocienfuegos <franco.cienfuegos@upeu.edu.pe>
2021-04-18 14:56:08 -0500

    first commit

---
+++ src/main/java/com/mycompany/prueba2/NewClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba2;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NewClass {
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 100; i++)
+        {
+            System.out.print("el resultasdo es "+ i + " = " + (100*i) + " ola k ace"+ "\n");
+        }
+    }
+}
